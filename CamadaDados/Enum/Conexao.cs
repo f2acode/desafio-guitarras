fbe20eca948d1463e4780f760a1ca88b2a56f229
Felipe augusto de almeida <augustto.felipe@hotmail.com>
2017-09-22 17:01:17 -0300

    corrigindo disposição do banco de dados

--- CamadaDados/Enum/Conexao.cs
+++ CamadaDados/Enum/Conexao.cs
@@ -1,7 +1,10 @@
-﻿namespace CamadaDados
+﻿
+namespace CamadaDados
 {
     class Conexao
     {
-        public static string cn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\projects\\desafio\\dotnet\\Guitarras.mdf;Integrated Security=True;";
+        //public static string cn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\projects\\desafio\\CamadaDados\\App_Data\\Guitarras.mdf;Integrated Security=True;";
+        public static string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Guitarras.mdf;Integrated Security=True;";
     }
+    
 }
